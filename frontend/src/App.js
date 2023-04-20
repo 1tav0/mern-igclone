f20d6a1eba8d743eb879d16fae277e54c94b3543
Tavo Cruz <madecruz2@gmail.com>
2023-04-19 23:03:50 -0700

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Header from './components/Header'
+  
 function App() {
   return (
-    <div className="App">
-      <h1>Wassup Yall!!!!</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css' 
+import Avatar from '@mui/material/Avatar';
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h3 className="logo">
+            Instagram
+        </h3>
+        <div className="right_side">
+            <p>Logout</p>
+            <Avatar 
+                className='profile_pic'
+                src='https://i.guim.co.uk/img/media/e5a60fe02429db588fdd910fcfa259b9c78b4e60/0_73_3313_1988/master/3313.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=72d54d2ff469eb5e148a278c0c7adc23'
+                alt='cr7'
+            />  
+        </div>
+    </div>
+  )
+}
+
+export default Header
