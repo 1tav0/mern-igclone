029e676aad1b65944d422a5c13191187f2bd0269
Tavo Cruz <madecruz2@gmail.com>
2023-04-20 23:32:29 -0700

    add firebase

---
+++ frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5BuHgShitbp4jAxammwyJWcBtTAUvWnI",
+    authDomain: "instagram-clone-ca3e7.firebaseapp.com",
+    projectId: "instagram-clone-ca3e7",
+    storageBucket: "instagram-clone-ca3e7.appspot.com",
+    messagingSenderId: "525072345544",
+    appId: "1:525072345544:web:4cb0d73f535f792c8938fd"
+  };
